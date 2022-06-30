8daf184205a26b4224d23688353142cf7f085730
Achille MBOUGUENG <achille.mbougueng@yahoo.fr>
2022-06-30 20:58:13 +0200

    Projet initial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { ref } from 'yup';
 
 function App() {
+  const [avis, setAvis] = React.useState();
+  const [typeAvis, setTypeAvis] = React.useState();
+  const refAvis  = React.useRef(null);
+
+  const handleAvis = () => {
+    const {current: {value}} = refAvis;
+    setAvis(value);
+    if(value.includes('bien')) {
+      setTypeAvis('positif');
+    } else{
+      setTypeAvis('negatif');
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='form-group'>
+          <input ref={refAvis} className='avis' name ="avis" placeholder='Merci de laisser votre avis'/>
+          <button type='button' onClick={handleAvis}>Enregistrer</button>
+        </div>
+        {
+          avis && typeAvis === 'positif' && (<p className='success'> Merci à nous motive à rester meilleur</p>)
+        }
+        {
+          avis && typeAvis === 'negatif' && (<p className='error'> C'est noté nous allons nous amméliorer</p>)
+        }
       </header>
     </div>
   );
